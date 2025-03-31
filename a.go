84fa57b0338f71134c122ecd8e819373135da014
Hemant <hemantjha2005@gmail.com>
2025-03-31 17:20:23 +0530

    started golang

---
+++ a.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!");
+    fmt.Println("This is a test file.");
+    fmt.Println("This is a test file 2.");
+}
---
+++ notes.md
@@ -0,0 +1,3 @@
+# Go lang
+
+## From Roadmap.sh
