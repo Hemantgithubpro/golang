7c296117a3fa8310ddf4bcb79f7512cd06fa7663
Hemant Jha <hemantjha2005@gmail.com>
2025-04-20 15:59:04 +0530

    for loop

---
+++ 1_Basics/D_Loops/forLoop/for.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main(){
+	// for i := 1; i < 5; i++ {
+	// 	fmt.Println("Hello World", i)
+	// }
+
+	for i:=range 5 {
+		fmt.Printf("Hello World->%d\n",i)
+	}
+}
---
+++ 1_Basics/D_Loops/whileLike/whileSimilar.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main(){
+	// in other languages, this is called a while loop
+	i:=0
+	for i < 5 {
+		fmt.Println("Hello World", i)
+		i++
+	}
+
+	// to behave like while(true) in other languages, use a for loop with no condition
+	// for { ...your code... }
+	
+	
+	
+}
