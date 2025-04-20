06ca6b5a49dcdf98188c1e856e61d78a8f83206b
Hemant Jha <hemantjha2005@gmail.com>
2025-04-20 16:02:47 +0530

    some formatting

---
+++ 1_Basics/D_Loops/whileLike/notes.md
@@ -0,0 +1,8 @@
+1. // Like a C for:
+`for init; condition; post { ... }`
+
+2. // Like a C while
+`for condition { ... }`
+
+3. // Like a C for(;;)
+`for { ... }`
--- 1_Basics/D_Loops/whileLike/whileSimilar.go
+++ 1_Basics/D_Loops/whileLike/whileSimilar.go
@@ -1,8 +1,10 @@
 package main
+
 import "fmt"
-func main(){
+
+func main() {
 	// in other languages, this is called a while loop
-	i:=0
+	i := 0
 	for i < 5 {
 		fmt.Println("Hello World", i)
 		i++
@@ -10,7 +12,5 @@ func main(){
 
 	// to behave like while(true) in other languages, use a for loop with no condition
 	// for { ...your code... }
-	
-	
-	
+
 }
